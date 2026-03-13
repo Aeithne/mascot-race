f383f5acd575f9dfa56eec3ede28bfd7e0cc42b8
Aeithne <40723680+Aeithne@users.noreply.github.com>
2026-03-12 22:40:14 -0300

    camera & gravity

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,13 +4,13 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
-
+@export var player_camera : Camera3D
 var direction = Vector3.ZERO
-
 var target_velocity = Vector3.ZERO
+@onready var camera_distance = player_camera.position - self.position
 
 func _physics_process(delta):
-
+	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
@@ -19,10 +19,16 @@ func _physics_process(delta):
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
+	
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+	player_camera.position = player_camera.position.lerp(position + camera_distance, delta)
+	player_camera.rotation = player_camera.rotation.lerp(player_camera.rotation * direction, delta)
 
 
 func _on_control_analogic_changed(value: Vector2, distance: float, angle: float, angle_clockwise: float, angle_not_clockwise: float) -> void:
